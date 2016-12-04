665c14ab6b7278adb5e26816a79c033801cf5737
CJ Sampson <cjsampson41@gmail.com>
2016-12-04 13:55:29 -0700

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,41 @@
+const webpack = require('webpack');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const combineLoaders = require('webpack-combine-loaders');
+
+
+module.exports = {
+   devtool: 'source-map',
+   entry: './resources/assets/js/components/Main.jsx',
+   output: {
+       filename: './public/js/main.js',
+   },
+   plugins: [
+       new ExtractTextPlugin('styles-[hash].css'),
+   ],
+   resolve: {
+       extensions: ['.js', '.jsx', '.json'],
+   },
+   module: {
+       loaders: [
+           // js loader
+           {
+               test: /\.jsx?$/,
+               exclude: /node_modules/,
+               loader: 'babel-loader',
+               query: {
+                   presets: ['es2015', 'react', 'stage-0'],
+                    plugins: ['transform-class-properties', 'transform-decorators-legacy'],
+               },
+           },
+           {
+               test: /(\.css|\.scss|\.sass)$/,
+               loaders: [
+                   'style-loader',
+                   'css-loader?sourceMap',
+                   'postcss-loader',
+                   'sass-loader?sourceMap',
+               ],
+           },
+       ],
+   },
+};
