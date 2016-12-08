ea1530cf87cbed13c41c6e48b8f823febba91866
CJ Sampson <cjsampson41@gmail.com>
2016-12-08 15:41:45 -0700

    portfolio data created for export

---
+++ resources/assets/js/src/data/portfolio.js
@@ -0,0 +1,50 @@
+[
+    {
+        image: "/images/bl.png",
+        imageAlt: "Blueline Screening Picture",
+        project: "Blueline Screening Source",
+        description: "Blueline Screening Source is a large, enterprise drug testing software.  I was tasked with building a user-based role system for the admin and client portals for access restriction.  The software had a complex backend with a large amount of sql tables.",
+        technologies: "Laravel Framework, Twitter Bootstrap, AJAX, Datatables, jQuery, Sequel Pro, NPM, Gulp, Vagrant, Ansible",
+        status: "Private Repo"
+    },
+    {
+        image: "/images/tandc.png", 
+        imageAlt: "TandC Application Picture",
+        project: "TandC",
+        description: "TandC is an application that has all sorts of resources from videos to articles all co-created by yours truely.  TandC uses Laravel for the backend while utilizing the Bulma CSS framework for layout/templating, jQuery for DOM maniputaion and user interaction, and many more open-source libraries.",
+        technologies: "Laravel Framework, SASS (CSS), jQuery, Bulma Framework, NPM, Gulp, Homestead",
+        status: "Private Repo"
+    },
+    {
+        image: "/images/sweetyams.png",
+        imageAlt: "Sweet Yams Picture",
+        project: "Sweet Yams",
+        description: "Description was a project that I was a part of for a local, organic restaurant.  The application had a CRUD build for the restaurant owners to update the menu.  Another big feature built for the application was the menu and ordering system.",
+        technologies: "Laravel Framework, Twitter Bootstrap, jQuery, Custom Flexbox, Vue.js, NPM, Gulp, Bower, Browsersync",
+        status: "Github"
+    },
+    {
+        image: "/images/wackamole.png",
+        imageAlt: "Wackamole Picture",
+        project: "Whack-a-mole",
+        description: "is a fun jQuery focused web game that has a basic score counter and random images that pop up on click.",
+        technologies: "HTML, CSS, JavaScript, jQuery",
+        status: "Github"
+    },
+    {
+        image: "/images/paramount.png",
+        imageAlt: "Parmount Accounting Picture",
+        project: "Paramount Accounting",
+        description: "is an accounting software to provide efficiency to a local Taxing business.  Roles performed for this project were to implement BDD using the Selenium Driver and Behat to provide Behavioral test in the browser.  Also a user-restriction feature was implemented to restrict user's from accesing certain features of the software.",
+        technologies: "HTML, CSS, JavaScript, jQuery, AJAX, Behat, BDD, Mink Extension, NPM, Bootstrap",
+        status: "Private Repo"
+    },
+    {
+        image: "/images/portfolio.png",
+        imageAlt: "cjsampson.me Picture",
+        project: "cjsampson.me" 
+        description: "is an application to host the projects that I have both built and been a team member of.  This app exercised my knowledge of React and functional programming to help better understand the core components of React.",
+        technologies: "HTML, CSS, Sass, JavaScript, Bulma, React, NPM, Webpack",
+        status: "Private Repo",
+    }
+]
