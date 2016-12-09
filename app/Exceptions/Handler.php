a2685637e27c5975ca298fe4296f7ae5dc5de07c
CJ Sampson <cjsampson41@gmail.com>
2016-12-09 13:10:59 -0700

    extra curly deletion

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -1,8 +1,11 @@
 <?php
+
 namespace App\Exceptions;
+
 use Exception;
 use Illuminate\Auth\AuthenticationException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+
 class Handler extends ExceptionHandler
 {
     /**
@@ -18,6 +21,7 @@ class Handler extends ExceptionHandler
         \Illuminate\Session\TokenMismatchException::class,
         \Illuminate\Validation\ValidationException::class,
     ];
+
     /**
      * Report or log an exception.
      *
@@ -30,6 +34,7 @@ class Handler extends ExceptionHandler
     {
         parent::report($exception);
     }
+
     /**
      * Render an exception into an HTTP response.
      *
@@ -39,8 +44,22 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        return parent::render($request, $exception);
+        if($this->isHttpException($exception)) {
+             switch ($exception->getStatusCode()) {
+                 case 404:
+                     return redirect()->route('/');
+ 
+                 case 500:
+                     return redirect()->route('/');
+ 
+                 default:
+                     return $this->renderHttpException($exception);
+             }
+         } else {
+             return parent::render($request, $exception);
+         }      
     }
+
     /**
      * Convert an authentication exception into an unauthenticated response.
      *
@@ -53,6 +72,7 @@ class Handler extends ExceptionHandler
         if ($request->expectsJson()) {
             return response()->json(['error' => 'Unauthenticated.'], 401);
         }
+
         return redirect()->guest('login');
     }
 }
