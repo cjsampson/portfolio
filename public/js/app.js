d99da0b2035c4426b0c4b7287be29ff244ea5516
CJ Sampson <cjsampson41@gmail.com>
2016-12-12 16:12:44 -0700

    class based About component converted to stateless

--- public/js/app.js
+++ public/js/app.js
@@ -14458,9 +14458,6 @@ module.exports = traverseAllChildren;
 Object.defineProperty(exports, "__esModule", {
     value: true
 });
-exports.default = undefined;
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 var _react = __webpack_require__(5);
 
@@ -14470,97 +14467,77 @@ __webpack_require__(263);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-var About = function (_Component) {
-    _inherits(About, _Component);
-
-    function About() {
-        _classCallCheck(this, About);
-
-        return _possibleConstructorReturn(this, (About.__proto__ || Object.getPrototypeOf(About)).apply(this, arguments));
-    }
-
-    _createClass(About, [{
-        key: 'render',
-        value: function render() {
-            return _react2.default.createElement(
+// implicit return
+var About = function About() {
+    return _react2.default.createElement(
+        'div',
+        { className: 'hero-body' },
+        _react2.default.createElement(
+            'div',
+            { className: 'container' },
+            _react2.default.createElement(
                 'div',
-                { className: 'hero-body' },
+                { className: 'columns column-row' },
                 _react2.default.createElement(
                     'div',
-                    { className: 'container' },
+                    { className: 'column is-half flex picture-column' },
+                    _react2.default.createElement(
+                        'figure',
+                        { className: 'image-container flex' },
+                        _react2.default.createElement('img', { src: '/images/coffeemug.jpg', alt: 'Image' })
+                    )
+                ),
+                _react2.default.createElement(
+                    'div',
+                    { className: 'column is-4 content-container' },
                     _react2.default.createElement(
                         'div',
-                        { className: 'columns column-row' },
+                        { className: 'content' },
                         _react2.default.createElement(
-                            'div',
-                            { className: 'column is-half flex picture-column' },
-                            _react2.default.createElement(
-                                'figure',
-                                { className: 'image-container flex' },
-                                _react2.default.createElement('img', { src: '/images/coffeemug.jpg', alt: 'Image' })
-                            )
+                            'h1',
+                            { className: 'about-heading has-text-centered' },
+                            'A Little About CJ?'
                         ),
                         _react2.default.createElement(
-                            'div',
-                            { className: 'column is-4 content-container' },
+                            'p',
+                            null,
+                            'My name is CJ Sampson and I am a software developer from Navarre Beach, Florida. Debugging complex problems related to anything programming or web is a great feeling.  Seeing software develop through the phases of nothing to production is one of the best feelings imaginable!'
+                        ),
+                        _react2.default.createElement(
+                            'p',
+                            null,
+                            'All things web/programming draw my interest being that I enjoy working on complex, logical problems.'
+                        ),
+                        _react2.default.createElement(
+                            'h2',
+                            { className: 'about-heading has-text-centered' },
+                            'Interests'
+                        ),
+                        _react2.default.createElement(
+                            'p',
+                            null,
+                            'Being that I\'m from the beach, the mountains in Salt Lake is where you\'ll be able to find me.'
+                        ),
+                        _react2.default.createElement(
+                            'p',
+                            null,
+                            'When I am not developing, I love reading anything related to Dr. Cal Newport, or listening to Dr. Rhonda Patrick.'
+                        ),
+                        _react2.default.createElement(
+                            'p',
+                            { className: 'value' },
                             _react2.default.createElement(
-                                'div',
-                                { className: 'content' },
-                                _react2.default.createElement(
-                                    'h1',
-                                    { className: 'about-heading has-text-centered' },
-                                    'A Little About CJ?'
-                                ),
-                                _react2.default.createElement(
-                                    'p',
-                                    null,
-                                    'My name is CJ Sampson and I am a software developer from Navarre Beach, Florida. Debugging complex problems related to anything programming or web is a great feeling.  Seeing software develop through the phases of nothing to production is one of the best feelings imaginable!'
-                                ),
-                                _react2.default.createElement(
-                                    'p',
-                                    null,
-                                    'All things web/programming draw my interest being that I enjoy working on complex, logical problems.'
-                                ),
-                                _react2.default.createElement(
-                                    'h2',
-                                    { className: 'about-heading has-text-centered' },
-                                    'Interests'
-                                ),
-                                _react2.default.createElement(
-                                    'p',
-                                    null,
-                                    'Being that I\'m from the beach, the mountains in Salt Lake is where you\'ll be able to find me.'
-                                ),
-                                _react2.default.createElement(
-                                    'p',
-                                    null,
-                                    'When I am not developing, I love reading anything related to Dr. Cal Newport, or listening to Dr. Rhonda Patrick.'
-                                ),
-                                _react2.default.createElement(
-                                    'p',
-                                    { className: 'value' },
-                                    _react2.default.createElement(
-                                        'strong',
-                                        null,
-                                        'Value > Passion'
-                                    )
-                                )
+                                'strong',
+                                null,
+                                'Value > Passion'
                             )
                         )
                     )
                 )
-            );
-        }
-    }]);
-
-    return About;
-}(_react.Component);
+            )
+        )
+    );
+};
 
 exports.default = About;
 
