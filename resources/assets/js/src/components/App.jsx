a27bc082010ccd1ce42227cd3acc16dc271c0a3e
CJ Sampson <cjsampson41@gmail.com>
2016-12-15 13:55:25 -0700

    google analytics

--- resources/assets/js/src/components/App.jsx
+++ resources/assets/js/src/components/App.jsx
@@ -19,7 +19,7 @@ class App extends Component {
                     
                 </div>
 
-                <Footer />
+                <Footer />                
             </div>
         );
     }
